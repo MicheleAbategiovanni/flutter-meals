9935f09fb504db5641a32d903cbb85baf4c20d9e
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2024-07-05 19:50:17 +0200

    add riverpod dependencies

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealFavorite = state.contains(meal);
+
+    if (mealFavorite) {
+      state = state.where((item) => item.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
